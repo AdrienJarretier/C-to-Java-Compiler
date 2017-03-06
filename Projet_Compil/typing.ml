0702e439ffdb80bef898f6b5448d99be5967d4c7
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-03-06 19:19:20 +0100

    all expr typed except CallE

--- Projet_Compil/typing.ml
+++ Projet_Compil/typing.ml
@@ -22,18 +22,39 @@ VarE (0, Var (Local , "n")),
 Const (0, IntV 2));;
 
 
+let exprTypeError = BinOp (0,
+BLogic BLand ,
+VarE (0, Var (Local , "n")),
+Const (0, IntV 2));;
+
+let exprTypeError2 = BinOp (0,
+BArith BAsub ,
+VarE (0, Var (Local , "n")),
+Const (0, BoolV false));;
+
+let nEgalKPlus1 = BinOp (0, BCompar BCeq , VarE (0, Var (Local , "n")),
+BinOp (0, BArith BAadd , VarE (0, Var (Local , "k")),
+Const (0, IntV 1)));;
+
+
+let ifThenElseExprTypeError = IfThenElse (0, Const (0, IntV 2), exprNmoins2, Const (0, IntV 2));;
+let ifThenElseExprTypeError2 = IfThenElse (0, nEgalKPlus1, exprNmoins2, nEgalKPlus1);;
+let ifThenElseExpr = IfThenElse (0, nEgalKPlus1, exprNmoins2, Const (0, IntV 2));;
+
+
 exception UndefinedVar of string;;
+exception TypeError of tp * tp;;
 
 
 
 let rec tp_expr env = function
-	  Const (_, v) -> let constType = function
+	  Const (0, v) -> let constType = function
 					  BoolV _ -> BoolT
 					| IntV _ -> IntT
 					| VoidV -> VoidT in
 	  		Const (constType v, v)
 
-	| VarE (_, Var(binding, vname)) -> let rec varType varName = function
+	| VarE (0, Var(binding, vname)) -> let rec varType varName = function
 									  (name, varT)::localvars ->
 									  		if name = varName
 			  								then
@@ -42,6 +63,35 @@ let rec tp_expr env = function
 			  									(varType varName localvars)
 									  | _ -> raise (UndefinedVar varName) in
 			VarE (varType vname env.localvar, Var(binding, vname))
+
+	| BinOp (0, op, exp1, exp2) -> let tp1 = tp_of_expr(tp_expr env exp1)
+									and tp2 = tp_of_expr(tp_expr env exp2) in
+			if tp1 = tp2
+			then
+				let checkTypeWithOp = function
+					  (BArith _, IntT) -> IntT
+					| (BLogic _, BoolT) -> BoolT
+					| (BCompar _, _) -> BoolT
+					| (op, expType) -> raise (TypeError (expType, expType))
+				in
+				BinOp (checkTypeWithOp(op, tp1), op, tp_expr env exp1, tp_expr env exp2)
+			else
+				raise (TypeError (tp1,tp2))
+
+	| IfThenElse (0, e1, e2, e3) -> let e1Typed = tp_expr env e1
+									and e2Typed = tp_expr env e2
+									and e3Typed = tp_expr env e3
+									in
+			let tp1=tp_of_expr(e1Typed) and tp2=tp_of_expr(e2Typed) and tp3=tp_of_expr(e3Typed) in
+				if tp1 = BoolT
+				then
+					if tp2 = tp3
+					then
+						IfThenElse (tp2, e1Typed, e2Typed, e3Typed)
+					else
+						raise (TypeError (tp2, tp3))
+				else
+					raise (TypeError (tp1, tp1))
 ;;
 
 (*
@@ -50,10 +100,28 @@ open Typing;;
 open Lang;;
 open Analyses;;
 
-tp_expr env (Const (0, IntV 2));;
+tp_expr;;
 
-tp_expr env (VarE (0, Var (Local , "n")));;
-tp_expr env (VarE (0, Var (Local , "notInEnvVar")));;
+let exprInEnv = tp_expr env;;
+
+exprInEnv (Const (0, IntV 2));;
+
+exprInEnv (VarE (0, Var (Local , "n")));;
+exprInEnv (VarE (0, Var (Local , "notInEnvVar")));;
+
+exprInEnv exprNmoins2;;
+exprInEnv exprTypeError;;
+exprInEnv exprTypeError2;;
+
+exprInEnv nEgalKPlus1;;
+
+tp_of_expr(exprInEnv nEgalKPlus1) = IntT;;
+tp_of_expr(exprInEnv nEgalKPlus1) = BoolT;;
+
+
+exprInEnv ifThenElseExprTypeError;;
+exprInEnv ifThenElseExprTypeError2;;
+exprInEnv ifThenElseExpr;;
 
 *)
 
