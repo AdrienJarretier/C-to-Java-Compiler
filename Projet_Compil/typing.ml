686195292821adbc4e66fc78d0e733c4595dfa38
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-03-10 00:13:21 +0100

    tabulations converted to 2 spaces

--- Projet_Compil/typing.ml
+++ Projet_Compil/typing.ml
@@ -50,72 +50,72 @@ exception NumberOfParametersNotMatching;;
 
 (* - : Typing.environment -> int Lang.expr -> Lang.tp Lang.expr = <fun> *)
 let rec tp_expr env = function
-	  (Const (_, v) : int expr) -> let constType = function
-					  BoolV _ -> BoolT
-					| IntV _ -> IntT
-					| VoidV -> VoidT in
-	  		Const (constType v, v)
+    (Const (_, v) : int expr) -> let constType = function
+            BoolV _ -> BoolT
+          | IntV _ -> IntT
+          | VoidV -> VoidT in
+        Const (constType v, v)
 
-	| VarE (_, Var(binding, vname)) -> let rec varType varName = function
-									  (name, varT)::localvars ->
-									  		if name = varName
-			  								then
-			  									varT
-			  								else
-			  									(varType varName localvars)
-									  | _ -> raise (UndefinedVar varName) in
-			VarE (varType vname env.localvar, Var(binding, vname))
+  | VarE (_, Var(binding, vname)) -> let rec varType varName = function
+                    (name, varT)::localvars ->
+                        if name = varName
+                        then
+                          varT
+                        else
+                          (varType varName localvars)
+                    | _ -> raise (UndefinedVar varName) in
+      VarE (varType vname env.localvar, Var(binding, vname))
 
-	| BinOp (_, op, exp1, exp2) -> let tp1 = tp_of_expr(tp_expr env exp1)
-									and tp2 = tp_of_expr(tp_expr env exp2) in
-			if tp1 = tp2
-			then
-				let checkTypeWithOp = function
-					  (BArith _, IntT) -> IntT
-					| (BLogic _, BoolT) -> BoolT
-					| (BCompar _, _) -> BoolT
-					| (op, expType) -> raise (TypeError (expType, expType))
-				in
-				BinOp (checkTypeWithOp(op, tp1), op, tp_expr env exp1, tp_expr env exp2)
-			else
-				raise (TypeError (tp1,tp2))
+  | BinOp (_, op, exp1, exp2) -> let tp1 = tp_of_expr(tp_expr env exp1)
+                  and tp2 = tp_of_expr(tp_expr env exp2) in
+      if tp1 = tp2
+      then
+        let checkTypeWithOp = function
+            (BArith _, IntT) -> IntT
+          | (BLogic _, BoolT) -> BoolT
+          | (BCompar _, _) -> BoolT
+          | (op, expType) -> raise (TypeError (expType, expType))
+        in
+        BinOp (checkTypeWithOp(op, tp1), op, tp_expr env exp1, tp_expr env exp2)
+      else
+        raise (TypeError (tp1,tp2))
 
-	| IfThenElse (_, e1, e2, e3) -> let e1Typed = tp_expr env e1
-									and e2Typed = tp_expr env e2
-									and e3Typed = tp_expr env e3
-									in
-			let tp1=tp_of_expr(e1Typed) and tp2=tp_of_expr(e2Typed) and tp3=tp_of_expr(e3Typed) in
-				if tp1 = BoolT
-				then
-					if tp2 = tp3
-					then
-						IfThenElse (tp2, e1Typed, e2Typed, e3Typed)
-					else
-						raise (TypeError (tp2, tp3))
-				else
-					raise (TypeError (tp1, tp1))
+  | IfThenElse (_, e1, e2, e3) -> let e1Typed = tp_expr env e1
+                  and e2Typed = tp_expr env e2
+                  and e3Typed = tp_expr env e3
+                  in
+      let tp1=tp_of_expr(e1Typed) and tp2=tp_of_expr(e2Typed) and tp3=tp_of_expr(e3Typed) in
+        if tp1 = BoolT
+        then
+          if tp2 = tp3
+          then
+            IfThenElse (tp2, e1Typed, e2Typed, e3Typed)
+          else
+            raise (TypeError (tp2, tp3))
+        else
+          raise (TypeError (tp1, tp1))
 
-	| CallE (_, fname, exprList) -> let rec funType = function
-																		(e::eList, param::pFunDecl) ->  let eTyped = (tp_expr env e) in
-																																		let tpExpr = tp_of_expr eTyped and tpParam = tp_of_vardecl param in
+  | CallE (_, fname, exprList) -> let rec funType = function
+                (e::eList, param::pFunDecl) ->  let eTyped = (tp_expr env e) in
+                                                let tpExpr = tp_of_expr eTyped and tpParam = tp_of_vardecl param in
 
-																																	if tpExpr = tpParam
-																																		then eTyped::funType(eList, pFunDecl)
-																																	else
-																																	raise (TypeError (tpExpr, tpParam))
-																	| ([], []) -> []
-																	| (_, _) -> raise NumberOfParametersNotMatching
-																		in
-																	let rec searchFunDecl = function
-																		(Fundecl (t, fn, pds))::funDecls ->
-																								if fn = fname
-																								then
-																									CallE (t, fn, funType (exprList, pds))
-																								else
-																									searchFunDecl funDecls
-																	| _ -> raise (UndefinedFun fname)
-																		in
-							searchFunDecl env.funbind
+                                              if tpExpr = tpParam
+                                                then eTyped::funType(eList, pFunDecl)
+                                              else
+                                              raise (TypeError (tpExpr, tpParam))
+              | ([], []) -> []
+              | (_, _) -> raise NumberOfParametersNotMatching
+                in
+              let rec searchFunDecl = function
+                (Fundecl (t, fn, pds))::funDecls ->
+                            if fn = fname
+                            then
+                              CallE (t, fn, funType (exprList, pds))
+                            else
+                              searchFunDecl funDecls
+              | _ -> raise (UndefinedFun fname)
+                in
+      searchFunDecl env.funbind
 ;;
 
 (*
