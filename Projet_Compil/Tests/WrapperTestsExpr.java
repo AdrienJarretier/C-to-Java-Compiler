5e1ccfd0dbb23dbabeef562f48fa2fc6d8215dbd
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-03-10 05:11:39 +0100

    wrapper for TestsExpr

---
+++ Projet_Compil/Tests/WrapperTestsExpr.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class WrapperTestsExpr {
+
+    public static void main(String[] args){
+
+        int i;
+        i = TestsExpr.test(5, 5);
+        System.out.println("result: "+ i);
+
+    }
+
+}
