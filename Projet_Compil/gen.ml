3b7ff67c70aa7ed34fdce9b88a6978126776d01a
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-03-10 02:13:19 +0100

    gen-expr returns a list of of instr

--- Projet_Compil/gen.ml
+++ Projet_Compil/gen.ml
@@ -4,18 +4,43 @@ open Lang
 open Analyses
 open Instrs
 
-(*
+(* *********************** TESTS ***********************
 
 #use "use.ml";;
 open Gen;;
+open Lang;;
 
+gen_expr;;
 
+gen_expr [("x", IntT); ("y", IntT)] exprXminusYplus2;;
 
  *)
+
+let exprXminusYplus2 =
+BinOp (IntT, BArith BAsub ,
+  VarE (IntT, Var (Local , "x")),
+  BinOp (IntT, BArith BAadd,
+    VarE (IntT, Var (Local , "y")),
+    Const (IntT, IntV 2)
+  )
+);;
 
 (* ************************************************************ *)
 (* **** Compilation of expressions / statements            **** *)
 (* ************************************************************ *)
+
+let rec position = function
+    (e, t::l) -> if t = e then 0 else 1+position(e, l)
+  | _ -> failwith "element not in list";;
+
+
+(*
+- : (Lang.vname * Lang.tp) list -> Lang.tp Lang.expr -> Instrs.instr list = <fun>
+*)
+let rec gen_expr varList = function
+    Const (t, v) -> [Loadc (t, v)]
+  | VarE (t,Var (_, name)) -> [Loadv (t, position((name, t), varList))]
+  | BinOp (tp, op, e1, e2) -> (gen_expr varList e1)@(gen_expr varList e2)@[Bininst (tp, op)];;
 
 
 
